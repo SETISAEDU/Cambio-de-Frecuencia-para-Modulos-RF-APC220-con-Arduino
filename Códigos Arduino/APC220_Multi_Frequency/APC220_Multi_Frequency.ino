65aa72608bd8fd57465a2f25b8530458173e4307
SETISAEDU <setisa2016@gmail.com>
2017-01-17 14:27:36 -0600

    Add files via upload

---
+++ Códigos Arduino/APC220_Multi_Frequency/APC220_Multi_Frequency.ino
@@ -0,0 +1,157 @@
+// TX
+const byte SetPin = 13; //Pin para poner el APC220 en modo de configuración
+const byte interruptPin = 2; //Selección de la Frecuencia
+
+volatile int Freq = 0;
+int Frecuencia1 = 420; //Primera frecuencia a configurar
+int Frecuencia2 = 435; //Segunda frecuencia a configurar
+int Frecuencia3 = 450; //Tercera frecuencia a configurar
+
+volatile byte Configured = LOW; //El Módulo ha sido configurado
+  
+volatile unsigned long last_micros;
+long debouncing_time = 175; //Debouncing Time in Milliseconds
+
+uint8_t data = 0,x = 0;
+
+void setup() {
+  Serial.begin(57600);
+  Serial1.begin(57600);
+  pinMode(SetPin, OUTPUT);
+
+  //Interrupciones
+  pinMode(2, INPUT_PULLUP);
+
+  //Mensaje Inicial
+  Serial.println();
+  Serial.println("Transmisor Multifrecuencia con APC220");
+  Serial.println("-------------------------------------");
+  Serial.println("Frecuencia inicial: "+String(Frecuencia1)+"MHz");
+
+  //Configuración Inicial del APC220
+  digitalWrite(SetPin, HIGH);
+  SelectFreq1();
+  delay(100);
+
+  //SelectFreq();
+  attachInterrupt(digitalPinToInterrupt(2), Interrupt1, FALLING);
+}
+
+void loop() {
+  if (Configured == LOW) {
+    if (Freq == 0) {
+      SelectFreq1();
+    }
+    else if (Freq == 1) {
+      SelectFreq2();
+    }
+    else if (Freq == 2) {
+      SelectFreq3();
+    }
+  }
+
+  Serial1.write(data);
+  delay(1);
+  x++;
+  if(x==100)
+  {
+    data++;
+    x=0;
+  }
+  if (data >= 7) {
+    data = 0;
+  }
+}
+
+void Interrupt1() {
+  if ((long)(micros() - last_micros) >= debouncing_time * 1000) {
+    last_micros = micros();
+    Configured = LOW;
+    Freq = Freq + 1;
+    if (Freq == 3) {
+      Freq = 0;
+    }
+  }
+}
+
+void SelectFreq1() {
+  // Procedimiento de configuración a la primera frecuencia
+  Serial.println();
+  Serial.println("-------------------------------------");
+  Serial.println("Configurando a " + String(Frecuencia1) + "MHz ...");
+  digitalWrite(SetPin, LOW);
+  delay(2);
+  Serial1.end();
+  Serial1.begin(9600);
+  Serial1.println("WR " + String(Frecuencia1) + "000 4 9 6 0");
+  delay(201);
+
+  if (Serial1.available())
+  {
+    String data = Serial1.readString();
+    Serial.println(data);
+  }
+  digitalWrite(SetPin, HIGH);
+  delay(11);
+  Serial1.end();
+  Serial1.begin(57600);
+  Serial.println("Configurado!!!");
+  Configured = HIGH;
+  Serial.println();
+  Serial.println("Transmitiendo a "+String(Frecuencia1)+"MHZ...");
+}
+
+void SelectFreq2() {
+  //Procedimiento de configuración a la segunda frecuencia
+  Serial.println();
+  Serial.println("-------------------------------------");
+  Serial.println("Configurando a " + String(Frecuencia2) + "MHz ...");
+  digitalWrite(SetPin, LOW);
+  delay(2);
+  Serial1.end();
+  Serial1.begin(9600);
+  Serial1.println("WR " + String(Frecuencia2) + "000 4 9 6 0");
+  delay(201);
+
+  if (Serial1.available())
+  {
+    String data = Serial1.readString();
+    Serial.println(data);
+  }
+  digitalWrite(SetPin, HIGH);
+  delay(11);
+  Serial1.end();
+  Serial1.begin(57600);
+  Serial.println("Configurado!!!");
+  Configured = HIGH;
+  Serial.println();
+  Serial.println("Transmitiendo a "+String(Frecuencia2)+"MHZ...");
+}
+
+void SelectFreq3() {
+  // Procedimiento de configuración a la tercera frecuencia
+  Serial.println();
+  Serial.println("-------------------------------------");
+  Serial.println("Configurando a " + String(Frecuencia3) + "MHz ...");
+  digitalWrite(SetPin, LOW);
+  delay(2);
+  Serial1.end();
+  Serial1.begin(9600);
+  Serial1.println("WR " + String(Frecuencia3) + "000 4 9 6 0");
+  delay(201);
+
+  if (Serial1.available())
+  {
+    String data = Serial1.readString();
+    Serial.println(data);
+  }
+  digitalWrite(SetPin, HIGH);
+  delay(11);
+  Serial1.end();
+  Serial1.begin(57600);
+  Serial.println("Configurado!!!");
+  Configured = HIGH;
+  Serial.println();
+  Serial.println("Transmitiendo a "+String(Frecuencia3)+"MHZ...");
+}
+
---
+++ Códigos Arduino/Receiver_apc220_435MHz/Receiver_apc220_435MHz.ino
@@ -0,0 +1,60 @@
+// RX 435MHz
+int LED3 = 5; //Conectar LED VERDE
+int LED2 = 6; //Conectar LED ROJO
+int LED1 = 7; //Conectar LED AMARILLO
+const byte SetPin = 4; //Pin para poner el APC220 en modo de configuración
+
+int Frecuencia = 435; //Ajustar frecuencia a de recepcion
+
+void setup() { 
+  Serial.begin(57600);
+  //Configuracion de pines para los LED como salidas
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(SetPin, OUTPUT);
+  
+  digitalWrite(SetPin, HIGH); //Se ajusta el Pin SET del modulo RF en Alto
+  SelectFreq(); //Configuracion del modulo
+  delay(100);
+  SelectFreq(); /* Segunda configuracion del modulo, 
+                 * que asegura que la frecuencia fue seteada de manera correcta
+                 */  
+}
+
+void loop() {
+  if (Serial.available()) {
+    int DataIn = Serial.read();
+    //Separacion de los valores de cada LED
+    bool LED1_s = DataIn & 0x04;
+    bool LED2_s = DataIn & 0x02;
+    bool LED3_s = DataIn & 0x01;
+    //Impresion de los valores recibidos
+    digitalWrite(LED1, LED1_s);
+    digitalWrite(LED2, LED2_s);
+    digitalWrite(LED3, LED3_s);
+  }
+}
+
+void SelectFreq() {
+  // Procedimiento de configuración de la frecuencia de recpecion
+  Serial.println();
+  Serial.println("Configurando a "+String(Frecuencia)+"MHz ...");
+  digitalWrite(SetPin, LOW);
+  delay(2);
+  Serial.end();
+  Serial.begin(9600);
+  Serial.println("WR "+String(Frecuencia)+"000 4 9 6 0");
+  delay(201);
+
+  if (Serial.available())
+  {
+    String data = Serial.readString();
+    Serial.println(data);
+  }
+  digitalWrite(SetPin, HIGH);
+  delay(11);
+  Serial.end();
+  Serial.begin(57600);
+  Serial.println("Configurado!!!");
+}
